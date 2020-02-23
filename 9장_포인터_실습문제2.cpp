27cda5f8c7b59f54647726288caa88860a1cf1c1
Blogsoul <42922298+blogSoul@users.noreply.github.com>
2020-02-24 01:16:37 +0900

    Update 9장_포인터_실습문제2.cpp

--- 9장_포인터_실습문제2.cpp
+++ 9장_포인터_실습문제2.cpp
@@ -90,11 +90,120 @@ int mainToPoint9To10() {
 	return 0;
 }
 
-int findeven(int arr[]) {
-
+int findeven(int *p,int N) {
+	int num = 0;
+	for (int i = 0; i < N; i++) {
+		if (*(p + i) % 2 == 0) {
+			num += 1;
+		}
+	}
+	return num;
 }
 
-int main(){
+int mainToPoint9To11(){
+	int N;
+	int arr[100];
+	int *p = arr;
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++) {
+		scanf("%d", (p + i));
+	}
+	printf("%d", findeven(p, N));
+	return 0;
+}
 
+void strcopy(char *p, char *q) {
+	for (int i = 0; i < 6; i++) {
+		*(p + i) = *(q + i);
+	}
+}
+
+int mainToPoint9To12() {
+	char a[10], b[10];
+	char *p= a, *q = b;
+	for (int i = 0; i < 6; i++) {
+		scanf("%c", (q + i));
+	}
+
+	strcopy(p, q);
+
+	for (int i = 0; i < 6; i++) {
+		printf("%c", *(p + i));
+	}
+	return 0;
+}
+
+void addArray(int *a, int *b, int *c, int N) {
+	for (int i = 0; i < N; i++) {
+		*(c + i) = *(a + i) + *(b + N - 1 - i);
+	}
+}
+
+int mainToPoint9To13() {
+	int N;
+	int a[10], b[10], c[10];
+	int *p = a, *q = b, *r = c;
+	scanf("%d", &N);
+	for (int i = 0;i<N; i++) {
+		scanf("%d", (p + i));
+	}
+	for (int i = 0; i < N; i++) {
+		scanf("%d", (q + i));
+	}
+	addArray(p, q, r, N);
+	for (int i = 0; i < N; i++) {
+		printf(" %d", *(r + i));
+	}
+	return 0;
+}
+
+void ABC(int *p ,int k) {
+	int max = *p, num, temp;
+	for (int i = 0; i < k; i++) {
+		if (max <= *(p + i)) {
+			max = *(p + i);
+			num = i;
+		}
+	}
+	temp = *p;
+	*p = *(p + num);
+	*(p + num) = temp;
+}
+
+int mainToPoint9To14() {
+	int arr[10];
+	int *p = arr;
+	for (int i = 0; i < 10; i++) {
+		scanf("%d", (p + i));
+	}
+	for (int i = 0; i < 9; i++) {
+		ABC((p + i), 10 - i);
+	}
+	for (int i = 0; i < 10; i++) {
+		printf(" %d", *(p + i));
+	}
+	return 0;
+}
+
+int arrsum(int *start, int *end) {
+	int sum = 0;
+	int *p;
+	for (p = start; p <= end; p++) {
+		sum += *p;
+	}
+	// 포인터 위치로 반복문에서 배열을 사용하는 예시이다.
+	return sum;
+}
+
+int mainToPoint9To15() {
+	int N, S, E;
+	int arr[100];
+	int *p = arr;
+
+	scanf("%d %d %d", &N, &S, &E);
+	for (int i = 0; i < N; i++) {
+		scanf("%d", (p + i));
+	}
+	printf("%d", arrsum((p + S), (p + E)));
 	return 0;
 }
