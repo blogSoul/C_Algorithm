bcdb2c5fe7e50efff4d2391a61b7613076bcd0f4
Blogsoul <42922298+blogSoul@users.noreply.github.com>
2020-02-27 03:13:46 +0900

    DONE

--- 10장_문자열_실습문제1.cpp
+++ 10장_문자열_실습문제1.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
 
 int maintoPoint10to1() {
 	char arr[20], str[20];
@@ -61,7 +62,142 @@ int maintoPoint10to3(){
 	return 0;
 }
 
-int main() {
+int maintoPoint10to4() {
+	char str[100], arr[100];
+	int i = 0, num, a = 0;
+	scanf("%s", str);
+	scanf("%s", arr);
+
+	for (i = 0; str[i]; ++i);
+	num = i;
+
+	for (i = 0; i < num; ++i) {
+		if (str[i] == arr[i]) {
+			a += 1;
+		}
+	}
+
+	if (a == num) {
+		a = 1;
+	}
+	else {
+		a = 0;
+	}
+
+	printf("%d %d\n", num, a);
+	return 0;
+}
+
+int maintoPoint10to5() {
+	char str1[20] = "\0";
+	char str2[20] = "\0";
+	// \0을 적용시켜 이후 가비지 값이 나오는 것을 방지합니다.
+	char *p = str1, *q = str2;
+	int i, N, j = 0;
+
+	scanf("%s", str1);
+	scanf("%s", str2);
+	scanf("%d", &N);
+
+	j = strlen(str2);
+	for (i = 0; str2[i]; ++i) {
+		str1[i + j + N] = str1[i + N];
+		str1[i + N] = str2[i];
+	}
+
+	printf("%s\n", str1);
+
+	return 0;
+}
+
+int maintoPoint10to6() {
+	char str1[20] = "\0";
+	char str2[20] = "\0";
+	// \0을 적용시켜 이후 가비지 값이 나오는 것을 방지합니다.
+	char *p = str1, *q = str2;
+	int i, N, j = 0, M;
+
+	scanf("%s", str1);
+	scanf("%s", str2);
+	scanf("%d", &N);
+	scanf("%d", &M);
+
+	j = strlen(str2);
+	if (M == 0) {
+		for (i = 0; str2[i]; ++i) {
+			str1[i + j + N] = str1[i + N];
+			str1[i + N] = str2[i];
+		}
+	}
+	else {
+		for (i = 0; str2[i]; ++i) {
+			str1[i + j + N] = str1[i + N];
+			str1[i + N] = str2[j - 1 - i];
+		}
+	}
+
+	printf("%s\n", str1);
+	return 0;
+}
+
+void member(int a, int b) {
+	if (b == 1) {
+		printf("one ");
+	}
+	else if (b == 2) {
+		printf("two ");
+	}
+	else if (b == 3) {
+		printf("three ");
+	}
+	else if (b == 4) {
+		printf("four ");
+	}
+	else if (b == 5) {
+		printf("five ");
+	}
+	else if (b == 6) {
+		printf("six ");
+	}
+	else if (b == 7) {
+		printf("seven ");
+	}
+	else if (b == 8) {
+		printf("eight ");
+	}
+	else if (b == 9) {
+		printf("nine ");
+	}
+
+	if (a == 3) {
+		printf("THO ");
+	}
+	else if (a == 2) {
+		printf("HUN ");
+	}
+	else if (a == 1) {
+		printf("TEN ");
+	}
+}
+
+int maintoPoint10to7() {
+	int N, M, a = 0, b, num, i;
+	scanf("%d", &N);
+	M = N;
+
+	while (true) {
+		N = N / 10;
+		if (N < 1)
+			break;
+		a += 1;
+	}
+
+	for (i = a; i >= 0; --i) {
+		num = pow(10, i);
+		b = M / num;
+		member(i, b);
+		M -= b * num;
+	}
 
 	return 0;
 }
