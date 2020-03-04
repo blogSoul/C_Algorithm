004fa5c0d0089b90ea8fbb7ab4a13f52647a3ccc
Blogsoul <42922298+blogSoul@users.noreply.github.com>
2020-03-04 11:09:59 +0900

    DONE

--- 12장_동적할당_실습문제2.cpp
+++ 12장_동적할당_실습문제2.cpp
@@ -86,10 +86,149 @@ int maintoPoint12to9() {
 	return 0;
 }
 
-int main() {
-	int N, i;
-	char *p = NULL;
+int maintoPoint12to10() {
+	int N, i, min, minCheck;
+	char **p = NULL, *q = NULL;
+	char str[100];
 	scanf("%d", &N);
+	getchar();
+	p = (char**)calloc(N, sizeof(char*));
+	for (i = 0; i < N; ++i) {
+		p[i] = (char*)calloc(100, sizeof(char));
+	}
+	for (i = 0; i < N; ++i) {
+		gets_s(str);
+		strcpy(p[i], str);
+	}
+
+	min = strlen(p[0]);
+	for (i = 0; i < N; ++i) {
+		if (min >= strlen(p[i])) {
+			minCheck = i;
+		}
+	}
+	puts(p[minCheck]);
+
+	for (i = 0; i < N; ++i) {
+		free(p[i]);
+	}
+	free(p);
+	return 0;
+}
+
+int maintoPoint12to11() {
+	int N, i, j, max, maxCheck;
+	char **p = NULL, *q = NULL;
+	char str[100];
+	scanf("%d", &N);
+	getchar();
+	p = (char**)calloc(N + 1, sizeof(char*));
+	for (i = 0; i < N + 1; ++i) {
+		p[i] = (char*)calloc(100, sizeof(char));
+	}
+	for (i = 0; i < N; ++i) {
+		gets_s(str);
+		strcpy(p[i], str);
+	}
+
 	
+	for (i = N - 1; i >= 0; --i) {
+		max = strlen(p[i]);
+		for (j = i; j < N; ++j) {
+			if (strlen(p[j]) <= strlen(p[j + 1])) {
+				p[N] = p[j];
+				p[j] = p[j + 1];
+				p[j + 1] = p[N];
+			}
+		}
+	}
+
+	for (i = 0; i < N; ++i) {
+		puts(p[i]);
+	}
+
+	for (i = 0; i < N; ++i) {
+		free(p[i]);
+	}
+	free(p);
+	return 0;
+}
+
+int maintoPoint12to12() {
+	int *p, i = 0, size = 5;
+	p = (int *)malloc(size * sizeof(int));
+	// sizeof(int)는 4이다.
+	while (size <= 20) {
+		if (i + 1 > size) {
+			size += 3;
+			p = (int *)realloc(p, size * sizeof(int));
+			scanf("%d", &p[i]);
+		}
+		else {
+			scanf("%d", &p[i]);
+		}
+		if (p[i] == -1) {
+			size = i;
+			for (i = 0; i <= size; ++i) {
+				printf(" %d", p[i]);
+			}
+			break;
+		}
+		i += 1;
+	}
+	free(p);
+	return 0;
+}
+
+int maintoPoint12to13() {
+	char **p, str[100];
+	int i, j, N, maxCheck = 0;
+	scanf("%d", &N);
+	getchar();
+	p = (char **)calloc(N + 1, sizeof(char*));
+	for (i = 0; i < N; ++i) {
+		gets_s(str);
+		p[i] = (char *)calloc(strlen(str) + 1, sizeof(char));
+		strcpy(p[i], str);
+	}
+
+	maxCheck = strlen(p[0]);
+	for (i = N - 1; i >= 0; --i) {
+		for (j = i - 1; j < N; ++j) {
+			if (maxCheck <= strlen(p[i])) {
+				maxCheck = strlen(p[i]);
+			}
+		}
+	}
+
+	for (i = 0; i <= N; ++i) {
+		p[i] = (char *)realloc(p[i], (maxCheck + 1) * sizeof(char));
+	}
+
+	for (i = N - 1; i >= 0; --i) {
+		for (j = i; j < N - 1; ++j) {
+			if (strcmp(p[j], p[j + 1]) == 1) {
+				p[N] = p[j];
+				p[j] = p[j + 1];
+				p[j + 1] = p[N];
+			}
+		}
+	}
+
+	for (i = 0; i < N; ++i) {
+		puts(p[i]);
+	}
+
+	for (i = 0; i < N; ++i) {
+		free(p[i]);
+	}
+	free(p);
+	/*
+	_CrtIsValidHeapPointer(block) 오류는 잘못된 포인터를 참조할 때 발생합니다. 
+	IsValidHeapPointer라는 어썰트는 말 그대로 해당 포인터가 valid(유효)한지 체크하는 겁니다.
+	여기서 말 하는 힙(Heap)은 현재 실행 중인 프로그램의 로컬 힙 메모리를 가리키는 것으로, 
+	즉, 해당 포인터가 참조하는 메모리 주소가 프로그램 상에서 new 등으로 할당한 공간의 주소인지를 
+	체크하는 것입니다.
+	*/
 	return 0;
 }
