08dde3132a3392eb222e504a418f51cd870d78b2
Blogsoul <42922298+blogSoul@users.noreply.github.com>
2020-03-07 16:35:41 +0900

    DONE

--- 13장_연산자함수자료형_이론문제.cpp
+++ 13장_연산자함수자료형_이론문제.cpp
@@ -8,7 +8,7 @@ int mainToPoint13to1() {
 	int x = 0X2A;
 	int y = 0XAD;
 	int z = x & y;
-	int a = x << 4; 
+	int a = x << 4;
 	// 비트 단위 이동 연산 : x를 비트 단위로 왼쪽으로 4만큼 이동
 	// 오른쪽 빈 자리는 k개의 0으로 채움
 
@@ -23,8 +23,8 @@ int mainToPoint13to2() {
 	unsigned char c;
 	int x, zs, zu;
 
-	printf("%#X %#X %#X\n", a&b, a | b, a^b);
-	printf("%#X %#X %#X\n", ~a, a<<2, a>>2);
+	printf("%#X %#X %#X\n", a & b, a | b, a ^ b);
+	printf("%#X %#X %#X\n", ~a, a << 2, a >> 2);
 
 	x = 0XA1234567; // x = 1010 0001 ...
 	zs = (signed)x >> 4; // 결과: 1111 1010 0001 ....
@@ -40,7 +40,7 @@ int mainToPoint13to2() {
 
 int mainToPoint13to3() {
 	int x = 0X71234567;
-	x = x >> 10; 
+	x = x >> 10;
 	// 오른쪽에서 10번째 비트를 가장 오른쪽에 위치시킴
 	x = x & 1;
 	// x와 ... 0001 의 AND
@@ -112,15 +112,15 @@ int mainToPoint13to5() {
 	return 0;
 }
 
-void swap2(int *p1, int *p2) {
-	int *tmp;
+void swap2(int* p1, int* p2) {
+	int* tmp;
 	tmp = p1;
 	p1 = p2;
 	p2 = tmp;
 }
 
-void swap3(int **pp1, int **pp2) {
-	int *ptmp;
+void swap3(int** pp1, int** pp2) {
+	int* ptmp;
 	ptmp = *pp1;
 	*pp1 = *pp2;
 	*pp2 = ptmp;
@@ -135,8 +135,8 @@ int mainToPoint13to6() {
 	printf("%d %p %p\n", num, pnum, ppnum);
 	*/
 	int num = 3;
-	int *pnum = &num; // 참조연산자를 한 개 사용하면 간접 참조를 한번(*)
-	int **ppnum = &pnum; // 참조연산자를 두 개 사용하면 간접 참조를 두번(**)
+	int* pnum = &num; // 참조연산자를 한 개 사용하면 간접 참조를 한번(*)
+	int** ppnum = &pnum; // 참조연산자를 두 개 사용하면 간접 참조를 두번(**)
 
 	printf("%p %p %d\n", ppnum, *ppnum, **ppnum);
 	printf("\n");
@@ -147,9 +147,9 @@ int mainToPoint13to6() {
 	printf("\n");
 
 	int x = 10, y = 20;
-	int *p1 = &x, *p2 = &y, *ptmp;
+	int* p1 = &x, * p2 = &y, * ptmp;
 
-	
+
 	ptmp = p1;
 	p1 = p2;
 	p2 = ptmp;
@@ -157,7 +157,7 @@ int mainToPoint13to6() {
 	printf("%d %d\n", *p1, *p2);
 	printf("\n");
 
-	swap2(p1, p2); 
+	swap2(p1, p2);
 	// 함수 안에 설정된 포인터는 함수가 마치고 메모리가 해제된다.
 	printf("%d %d\n", *p1, *p2);
 	printf("\n");
@@ -168,13 +168,113 @@ int mainToPoint13to6() {
 	return 0;
 }
 
-int main() {
+void init3(int x[][3]) {// int (*x)[3]과 동일
+	x[0][0] = x[1][0] = 0;
+}
+
+int(*init4(int(*x)[3]))[3]{
+	x[0][0] = x[1][0] = 0;
+    return x;
+}
+
+int mainToPoint13to7() {
 	int a = 1, b = 2, c = 3;
-	int *pi[3] = { &a, &b, &c }; // int포인터 배열
-	int **ppi;
+	int* pi[3] = { &a, &b, &c }; // int포인터 배열
+	int** ppi;
+	int i = 10, * qi, ** qqi, *** qqqi, **** qqqqi;
+	int arr[2][3] = { {-2, 4, 0},{1, 2, 3} }; // arr의 자료형은 int(*)[3]
+	int j = 0, ar[5] = { 2,3,5,7,-1 };
+	int at[3][5];
+	int* p, (*px)[5], (*py)[3][5]; // px는 "크기가 5인 int 배열"에 대한 포인터입니다.
+	// int *(px[5])와 동일 <- 연산자 []의 우선 순위가 *보다 높음
+	// 즉, 포인터들의 배열(포인터 배열)
+	int(*r)[3];
 
 	ppi = pi;
 	// 배열 이름 pi는 &pi[0]와 동일
 	printf("%p %p %d\n", ppi, *ppi, **ppi);
+	printf("%d %d %d\n", **ppi, **(ppi + 1), **(ppi + 2));
+	// int 배열로 주소지정을 해주면 주소들이 1만큼 차이나게 연속적으로 할당된다.
+	
+	qi = &i; // (int)형인 i의 주소는 (int *)형
+	qqi = &qi; // (int *)형인 qi의 주소는 (int **)형
+	qqqi = &qqi; // (int **)형인 qqi의 주소는 (int ***)형
+	qqqqi = &qqqi; // (int ***)형인 qqqi의 주소는 (int ****)형
+	printf("%d %d %d %d %d\n", i, *qi, **qqi, ***qqqi, ****qqqqi);
+
+	init3(arr);
+	printf("%d %d %d\n", arr[0][0], arr[0][1], arr[0][2]);
+	printf("%d %d %d\n", arr[1][0], arr[1][1], arr[1][2]);
+
+	p = &j; // int형에 대한 포인터(int *)
+	p = ar; // == &ar[0], 즉 int형에 대한 포인터(int *)
+	//p = &ar;
+	// "int *"의 간접 참조 수준이 'int(*)[5]'와 다르다
+	px = &ar; 
+	printf("%p %p\n", p, px);
+	printf("%p %p %p\n", p, p + 1, p + 2); // 주소값이 4만큼 증가 
+	printf("%p %p %p\n", px, px + 1, px + 2); // 주소값이 20만큼 증가
+
+	p = at[1];
+
+	px = &at[1];
+	//py = at; py는 자료형을 맞출 수 없다.
+	r = init4(arr);
+	printf("%d %d %d\n", arr[0][0], arr[0][1], arr[0][2]);
+	printf("%d %d %d\n", arr[1][0], arr[1][1], arr[1][2]);
+	return 0;
+}
+
+void print(int a, int b) {
+	printf("%d %d\n", a, b);
+}
+
+int add(int a, int b) {
+	return a + b;
+}
+
+int sub(int a, int b) {
+	return a - b;
+}
+
+union student6 {// 공용체
+	int id;
+	char name[8]; 
+	// 모든 멤버가 메모리를 공유한다.
+	// 따라서 데이터를 변경하면 다른 데이터에 영향을 미친다.
+};
+
+enum day_type {// 열거형
+	sun, mon, tue, wed, thu, fri, sat
+	// 이름이 부여된 정수 상수의 집합
+	// 코드의 가독성을 높여줌
+	// 내부적으로는0부터 차례로 멤버들에세 정수가 부여됨
+};
+
+int mainToPoint13to8() {
+	int x[2] = { 4, 8 };
+	void* p = x;
+	void (*fp)(int, int);
+	// 반환형 void, 인자가 int형 2개인 함수 포인터 연결
+	char op; int a, b;
+	int (*fq)(int, int);
+	
+	printf("%d\n", *(int*)p);
+	p = (int*)p + 1; 
+	printf("%d\n", *(int*)p);
+
+	fp = print;
+	print(2, 3);
+	fp(1, 5);
+
+	scanf("%c", &op); 
+	scanf("%d %d", &a, &b);
+	if (op == '+')
+		fq = add;
+	else if (op == '-')
+		fq = sub;
+	else
+		return 0;
+	printf("=> %d\n", fq(a, b));
 	return 0;
 }
