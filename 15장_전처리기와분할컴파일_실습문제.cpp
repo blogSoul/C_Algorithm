5ce244ce5121e2f34961a45e85937162b4391fc1
Blogsoul <42922298+blogSoul@users.noreply.github.com>
2020-03-10 06:27:34 +0900

    DONE

--- 15장_전처리기와분할컴파일_실습문제.cpp
+++ 15장_전처리기와분할컴파일_실습문제.cpp
@@ -2,8 +2,20 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define ABS(a) (a>0?a:-a)
+#define MAX(a,b) (a>b?a:b)
 
 int main() {
-
+	int num[6], i, max, abs;
+	for (i = 0; i < 6; ++i) {
+		scanf("%d", &num[i]);
+	}
+	max = num[0];
+	abs = num[0];
+	for (i = 0; i < 6; ++i) {
+		max = MAX(max, num[i]);
+		abs = (ABS(num[i]) > ABS(abs) ? num[i]:abs);
+	}
+	printf("%d %d", abs, ABS(abs));
 	return 0;
 }
