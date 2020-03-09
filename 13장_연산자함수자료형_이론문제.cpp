c7fa34b5ccfd37845f787c92e2af3e7b2a1fb3c7
Blogsoul <42922298+blogSoul@users.noreply.github.com>
2020-03-09 22:39:41 +0900

    DONE

--- 13장_연산자함수자료형_이론문제.cpp
+++ 13장_연산자함수자료형_이론문제.cpp
@@ -1,280 +1,165 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h> // rand 함수 사용을 위해
-#include <time.h> // time 함수 사용을 위해
+#include <stdlib.h>
+#include <math.h>
 
-int mainToPoint13to1() {
-	// 비트 단위 논리 연산자 : x & y
-	int x = 0X2A;
-	int y = 0XAD;
-	int z = x & y;
-	int a = x << 4;
-	// 비트 단위 이동 연산 : x를 비트 단위로 왼쪽으로 4만큼 이동
-	// 오른쪽 빈 자리는 k개의 0으로 채움
+// 파일 입출력에 필요한 txt, dat 파일은 따로 첨부하지 않았습니다.
 
-	printf("%#X\n", z);
-	printf("%#X\n", a);
-	return 0;
-}
+int mainToPoint14to1() {
+	// stream : 프로그램과 파일 사이에 교환되는 연속적인 바이트 흐름(데이터의 논리적 흐름)
+	FILE *fp, *fq;
+	int i, N , num = 10;
 
-int mainToPoint13to2() {
-	unsigned char a = 01101100;
-	unsigned char b = 01001010;
-	unsigned char c;
-	int x, zs, zu;
+	fq = fopen("test.txt", "w");
+	if (fq == NULL) {
+		printf("Couldn't open file!\n");
+		return -1;
+		// EOF(End Of File) : 파일의 끝을 표현하기 위해 정의해 놓은 상수(즉, -1)
+	}
+	for (i = 0; i < 3; ++i) {
+		fprintf(fq, "%d\n", i);
+	}
+	fprintf(fq, "First file test!\n");
+	fclose(fq);
 
-	printf("%#X %#X %#X\n", a & b, a | b, a ^ b);
-	printf("%#X %#X %#X\n", ~a, a << 2, a >> 2);
-
-	x = 0XA1234567; // x = 1010 0001 ...
-	zs = (signed)x >> 4; // 결과: 1111 1010 0001 ....
-	zu = (unsigned)x >> 4; // 결과: 0000 1010 0001 .....
-	printf("signed = %#X, unsigned = %#X\n", zs, zu);
-
-	x = 0X71234567; // x = 1010 0001 ...
-	zs = (signed)x >> 4; // 결과: 0000 0111 0001 ....
-	zu = (unsigned)x >> 4; // 결과: 0000 0111 0001 .....
-	printf("signed = %#X, unsigned = %#X\n", zs, zu);
-	return 0;
-}
-
-int mainToPoint13to3() {
-	int x = 0X71234567;
-	x = x >> 10;
-	// 오른쪽에서 10번째 비트를 가장 오른쪽에 위치시킴
-	x = x & 1;
-	// x와 ... 0001 의 AND
-	// 두 문장을 하나로 합하면 (x >> 10) & 1
-
-	printf("%d\n", x);
-	return 0;
-}
-
-void dec1(int x) {
-	printf("x: %d\n", x);
-	if (x > 1)
-		dec1(x - 1);
-}
-
-void dec2(int x) {
-	printf("+start: x=%d\n", x);
-	if (x > 1) dec2(x - 1);
-	printf("-end: x=%d\n", x);
-}
-
-int sum(int n) {
-	if (n == 1) return 1;
-	return sum(n - 1) + n;
-}
-
-int factorial(int n) {
-	if (n == 1) return 1;
-	return factorial(n - 1) * n;
-}
-
-int mainToPoint13to4() {
-	dec1(3);
-	printf("\n");
-	dec2(3);
-	printf("\n");
-	printf("%d\n", sum(10));
-	printf("%d\n", factorial(10));
-	printf("%d\n", rand()); // 난수 생성
-	srand(10); // 시드를 10으로 지정
-	printf("%d\n", rand()); // 난수 생성
-	srand(time(NULL)); // 시드를 현재시간으로 지정
-	printf("%d\n", rand()); // 난수 생성
-	return 0;
-}
-
-int randomnumber1() {
-	int n;
-	n = rand() % 100;
-	return n;
-}
-
-int mainToPoint13to5() {
-	int i;
-	for (i = 0; i < 5; i++) {
-		printf("%d\n", randomnumber1());
+	fp = fopen("test.txt", "r");
+	// 만약 경로로 설정하고 싶으면 "C:\\C_pro\\Project\\test.dat"와 같이 \\로 경로를 설정해줍니다.
+	// r: 읽기, w: 쓰기, a: 추가
+	if (fp == NULL) {
+		printf("Couldn't open file!\n");
+		return -1;
+		// EOF(End Of File) : 파일의 끝을 표현하기 위해 정의해 놓은 상수(즉, -1)
 	}
 
-	clock_t start, finish;
-	double duration;
+	for (i = 0; i < 3; ++i) {
+		fscanf(fp, "%d", &N); // 스캔을 할 대상을 확인해야 한다.
+		printf("%d\n", N);
+	}
 
-	start = clock(); // 시작 시각
-	// 수행시간을 측정하고 하는 코드..!
-	finish = clock(); // 종료 시각
-
-	duration = (double)(finish - start) / CLOCKS_PER_SEC;
-	// CLOCKS_PER_SEC (time.h에 정의된 상수)단위로 반환
-	printf("실행 시간: %lf 초 \n", duration);
 	return 0;
 }
 
-void swap2(int* p1, int* p2) {
-	int* tmp;
-	tmp = p1;
-	p1 = p2;
-	p2 = tmp;
-}
+int mainToPoint14to2() {
+	FILE *fp1, *fp2;
+	char ch;
+	char str[100];
 
-void swap3(int** pp1, int** pp2) {
-	int* ptmp;
-	ptmp = *pp1;
-	*pp1 = *pp2;
-	*pp2 = ptmp;
-}
-
-int mainToPoint13to6() {
-	/*
-	int num = 3, *pnum, **ppnum;
-	pnum = &num; // 변수 num의 주소가 pnum에 저장됨
-	ppnum = &pnum; // 변수 pnum의 주소가 ppnum에 저장됨
-
-	printf("%d %p %p\n", num, pnum, ppnum);
-	*/
-	int num = 3;
-	int* pnum = &num; // 참조연산자를 한 개 사용하면 간접 참조를 한번(*)
-	int** ppnum = &pnum; // 참조연산자를 두 개 사용하면 간접 참조를 두번(**)
-
-	printf("%p %p %d\n", ppnum, *ppnum, **ppnum);
+	fp1 = fopen("input.txt", "r");
+	if (fp1 == NULL) {
+		printf("Couldn't open file!");
+		return -1;
+	}
+	fp2 = fopen("output.txt", "w");
+	if (fp2 == NULL) {
+		printf("Couldn't open file!");
+		return -1;
+	}
+	while ((ch = fgetc(fp1)) != EOF) {
+		printf("%c", ch);
+		fputc(ch, fp2);
+	}
 	printf("\n");
+	fclose(fp1);
+	fclose(fp2);
 
-	printf("%p %d\n", &num, num);
-	printf("%p %p %d\n", &pnum, pnum, *pnum);
-	printf("%p %p %p %d\n", &ppnum, ppnum, *ppnum, **ppnum);
-	printf("\n");
+	fp1 = fopen("input.txt", "r");
+	if (fp1 == NULL) {
+		printf("Couldn't open file!");
+		return -1;
+	}
+	fp2 = fopen("output.txt", "w");
+	if (fp2 == NULL) {
+		printf("Couldn't open file!");
+		return -1;
+	}
+	while (fgets(str, sizeof(str), fp1) != NULL) {
+		printf("%s\n", str);
+		fputs(str, fp2);
+	}
+	fclose(fp1);
+	fclose(fp2);
 
-	int x = 10, y = 20;
-	int* p1 = &x, *p2 = &y, *ptmp;
-
-
-	ptmp = p1;
-	p1 = p2;
-	p2 = ptmp;
-	// p1 <-> p2 교환
-	printf("%d %d\n", *p1, *p2);
-	printf("\n");
-
-	swap2(p1, p2);
-	// 함수 안에 설정된 포인터는 함수가 마치고 메모리가 해제된다.
-	printf("%d %d\n", *p1, *p2);
-	printf("\n");
-
-	swap3(&p1, &p2); // p1 <-> p2 교환
-	printf("%d %d\n", *p1, *p2); // p1, p2의 주소는 (int **)형이다.
-	printf("\n");
+	// 반환값 참조:
+	// fgetc() : 파일 끝에서 EOF(-1) 반환
+	// fgets() : 파일 끝에서 NULL(0) 반환
+	// fscanf() : 파일 끝에서 EOF(-1) 반환
+	
 	return 0;
 }
 
-void init3(int x[][3]) {// int (*x)[3]과 동일
-	x[0][0] = x[1][0] = 0;
-}
+int mainToPoint14to3() {
+	FILE *fp;
+	char str[100];
 
-int(*init4(int(*x)[3]))[3]{
-	x[0][0] = x[1][0] = 0;
-	return x;
-}
-
-int mainToPoint13to7() {
-	int a = 1, b = 2, c = 3;
-	int* pi[3] = { &a, &b, &c }; // int포인터 배열
-	int** ppi;
-	int i = 10, *qi, ** qqi, *** qqqi, **** qqqqi;
-	int arr[2][3] = { {-2, 4, 0},{1, 2, 3} }; // arr의 자료형은 int(*)[3]
-	int j = 0, ar[5] = { 2,3,5,7,-1 };
-	int at[3][5];
-	int* p, (*px)[5], (*py)[3][5]; // px는 "크기가 5인 int 배열"에 대한 포인터입니다.
-	// int *(px[5])와 동일 <- 연산자 []의 우선 순위가 *보다 높음
-	// 즉, 포인터들의 배열(포인터 배열)
-	int(*r)[3];
-
-	ppi = pi;
-	// 배열 이름 pi는 &pi[0]와 동일
-	printf("%p %p %d\n", ppi, *ppi, **ppi);
-	printf("%d %d %d\n", **ppi, **(ppi + 1), **(ppi + 2));
-	// int 배열로 주소지정을 해주면 주소들이 1만큼 차이나게 연속적으로 할당된다.
-
-	qi = &i; // (int)형인 i의 주소는 (int *)형
-	qqi = &qi; // (int *)형인 qi의 주소는 (int **)형
-	qqqi = &qqi; // (int **)형인 qqi의 주소는 (int ***)형
-	qqqqi = &qqqi; // (int ***)형인 qqqi의 주소는 (int ****)형
-	printf("%d %d %d %d %d\n", i, *qi, **qqi, ***qqqi, ****qqqqi);
-
-	init3(arr);
-	printf("%d %d %d\n", arr[0][0], arr[0][1], arr[0][2]);
-	printf("%d %d %d\n", arr[1][0], arr[1][1], arr[1][2]);
-
-	p = &j; // int형에 대한 포인터(int *)
-	p = ar; // == &ar[0], 즉 int형에 대한 포인터(int *)
-	//p = &ar;
-	// "int *"의 간접 참조 수준이 'int(*)[5]'와 다르다
-	px = &ar;
-	printf("%p %p\n", p, px);
-	printf("%p %p %p\n", p, p + 1, p + 2); // 주소값이 4만큼 증가 
-	printf("%p %p %p\n", px, px + 1, px + 2); // 주소값이 20만큼 증가
-
-	p = at[1];
-
-	px = &at[1];
-	//py = at; py는 자료형을 맞출 수 없다.
-	r = init4(arr);
-	printf("%d %d %d\n", arr[0][0], arr[0][1], arr[0][2]);
-	printf("%d %d %d\n", arr[1][0], arr[1][1], arr[1][2]);
+	fp = fopen("data.txt", "r");
+	if (fp == NULL) {
+		printf("Couldnt open file!");
+		return -1;
+	}
+	fgets(str, sizeof(str), fp);
+	while (!feof(fp)) {
+		printf("%s", str);
+		fgets(str, sizeof(str), fp);
+	}
+	// ^Z를 지나가야 feof(fp) 함수는 0이 아닌 값(참)을 반환합니다.
+	fclose(fp);
+	// feof() 함수는 EOF를 만났을 떄 0을 반환한다.
 	return 0;
 }
 
-void print(int a, int b) {
-	printf("%d %d\n", a, b);
-}
-
-int add2(int a, int b) {
-	return a + b;
-}
-
-int sub(int a, int b) {
-	return a - b;
-}
-
-union student6 {// 공용체
-	int id;
+struct person {
 	char name[8];
-	// 모든 멤버가 메모리를 공유한다.
-	// 따라서 데이터를 변경하면 다른 데이터에 영향을 미친다.
+	int age;
 };
 
-enum day_type {// 열거형
-	sun, mon, tue, wed, thu, fri, sat
-	// 이름이 부여된 정수 상수의 집합
-	// 코드의 가독성을 높여줌
-	// 내부적으로는0부터 차례로 멤버들에세 정수가 부여됨
-};
+int mainToPoint14to4() {
+	FILE *fp;
+	char mybuf[30];
+	struct person buf[10];
+	struct person data[10] = { {"Tom", 46}, {"James", 33}, {"Jane", 21} };
+	int i, size;
 
-int mainToPoint13to8() {
-	int x[2] = { 4, 8 };
-	void* p = x;
-	void(*fp)(int, int);
-	// 반환형 void, 인자가 int형 2개인 함수 포인터 연결
-	char op; int a, b;
-	int(*fq)(int, int);
+	fp = fopen("data.txt", "wb");
+	if (fp == NULL) {
+		printf("Couldn't open file!");
+		return -1;
+	}
+	fwrite(data, sizeof(struct person), 3, fp);
+	fclose(fp);
 
-	printf("%d\n", *(int*)p);
-	p = (int*)p + 1;
-	printf("%d\n", *(int*)p);
+	fp = fopen("data.txt", "rb");
+	if (fp == NULL) {
+		printf("Couldn't open file!");
+		return -1;
+	}
+	fread(buf, sizeof(struct person), 3, fp);
+	for (i = 0; i <= 2; ++i) {
+		printf("i = %d %s %d\n", i, buf[i].name, buf[i].age);
+	}
+	fclose(fp);
 
-	fp = print;
-	print(2, 3);
-	fp(1, 5);
+	fp = fopen("data.txt", "rb");
+	if (fp == NULL) {
+		printf("Couldn't open file!");
+		return -1;
+	}
+	fseek(fp, 0, SEEK_END);
+	size = ftell(fp);
+	fclose(fp);
+	printf("Size fo the file: %d bytes. \n", size);
 
-	scanf("%c", &op);
-	scanf("%d %d", &a, &b);
-	if (op == '+')
-		fq = add2;
-	else if (op == '-')
-		fq = sub;
-	else
-		return 0;
-	printf("=> %d\n", fq(a, b));
+	// buffer flush : 필요에 의해 버퍼의 내용을 강제로 파일에 출력하고, 버퍼를 비우는 일
+	fp = fopen("data.txt", "w+");
+	if (fp == NULL) {
+		printf("Couldn't open file!");
+		return -1;
+	}
+	fputs("Remove data (fflush) ", fp);
+	fflush(fp);
+	// buffer flush : 필요에 의해 버퍼의 내용을 강제로 파일에 출력하고, 버퍼를 비우는 일
+	rewind(fp);
+	fgets(mybuf, 10, fp);
+	puts(mybuf);
+	fclose(fp);
 	return 0;
 }
