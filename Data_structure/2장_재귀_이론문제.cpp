da89192c621130539a21808a8e94ff2027523456
Blogsoul <42922298+blogSoul@users.noreply.github.com>
2020-03-16 03:24:33 +0900

    Create 2장_재귀_이론문제.cpp

---
+++ Data_structure/2장_재귀_이론문제.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+	// 알고리즘 자신을 사용하여 정의된 알고리즘을 재귀적이라고 말한다.
+	// 재귀적으로 해결되어야 할 경우, 재귀호출은 항상 베이스 케이스를 향하는
+	// 방향으로 진행되어야 한다.
+
+	return 0;
+}
