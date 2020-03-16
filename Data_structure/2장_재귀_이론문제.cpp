92437175a27d278638bcf1cfa9430557573a2bce
Blogsoul <42922298+blogSoul@users.noreply.github.com>
2020-03-17 01:47:23 +0900

    DONE

--- Data_structure/2장_재귀_이론문제.cpp
+++ Data_structure/2장_재귀_이론문제.cpp
@@ -2,10 +2,29 @@
 #include <stdlib.h>
 #include <string.h>
 
+int product(int a, int b) {
+	if (b == 1)
+		return a;
+	return a + product(a, b - 1);
+}
+int modulo(int a, int b) {
+	if (a < b)
+		return a;
+	else
+		return modulo(a - b, b);
+}
+int quotient(int a, int b) {
+	if (a < b)
+		return a;
+	else
+		return 1 + quotient(a - b, b);
+}
+
 int main() {
 	// 알고리즘 자신을 사용하여 정의된 알고리즘을 재귀적이라고 말한다.
 	// 재귀적으로 해결되어야 할 경우, 재귀호출은 항상 베이스 케이스를 향하는
 	// 방향으로 진행되어야 한다.
 
+	// hanoi탑 n개를 이동시키려면 (2**n)-1번 이동시켜야 합니다.
 	return 0;
 }
