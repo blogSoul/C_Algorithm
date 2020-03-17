5231d081aa0e359fde786cef1528b1883f49794b
Blogsoul <42922298+blogSoul@users.noreply.github.com>
2020-03-17 18:58:38 +0900

    Update 3장_기초데이터구조_이론문제.cpp

--- Data_structure/Data_structure_re/3장_기초데이터구조_이론문제.cpp
+++ Data_structure/Data_structure_re/3장_기초데이터구조_이론문제.cpp
@@ -3,6 +3,17 @@
 #include <string.h>
 
 int main() {
+	// 데이터 구조의 기본재료 : 배열, 연결리스트
 
+	// 배열 : 
+	// 배열첨자(array index) : 시작: 0 또는 LB(lower bound)
+	//                       : 끝: N-1 또는 UB(upper bound)
+	// 배열표시(array denotation) : V[LB..UB]
+	// V[LB..UB]의 base로부터 V[k]의 offset = k -LB
+	// 다차원 배열 :
+	// 배열표시 : LB = 0이면, V[N1xN2...xNn]
+	//            일반적으로, V[LB1..UB1, LB2..UB2, ... , LBn..UBn]
+
+	// 메모리 할당 : 행우선 순서(또는 저차원우선 순서)
 	return 0;
 }
