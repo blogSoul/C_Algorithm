2d13ddf5817914af48676b21967b6a904e0fb6bc
Blogsoul <42922298+blogSoul@users.noreply.github.com>
2020-03-22 23:53:03 +0900

    Update 3장_재귀_실습문제.cpp

--- Data_structure/DataStructure/3장_재귀_실습문제.cpp
+++ Data_structure/DataStructure/3장_재귀_실습문제.cpp
@@ -1,1 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
+int sum2(int num) {
+	if (num == 1)
+		return 1;
+	return num + sum2(num - 1);
+}
+
+int main3to1() {
+	int num;
+	scanf("%d", &num);
+	num = sum2(num);
+	printf("%d\n", num);
+	return 0;
+}
+
+int main3to2() {
+	int num, N, cnt, i;
+	scanf("%d", &num);
+	N = num;
+	for (cnt = 0;; ++cnt) {
+		if (N < pow(10, cnt)) {
+			break;
+		}
+	}
+	if (N == 0) {
+		printf("%d\n", N);
+		return 0;
+	}
+	for (i = cnt-1;0 <= i; --i) {
+		N = num / pow(10, i);
+		printf("%d\n", N);
+		num -= N * pow(10, i);
+	}
+	return 0;
+}
+
+void digitAndNumber(int n) {
+	if (n == 0) {
+		return;
+	}
+	//printf("%d\n", n / 10);
+	printf("%d\n", n % 10);
+	return digitAndNumber(n / 10);
+}
+
+int main3to3() {
+	int num, N, cnt, i;
+	scanf("%d", &num);
+	N = num;
+	digitAndNumber(num);
+	return 0;
+}
+
+int maxArr(int arr[]) {
+	int num;
+	num = sizeof(arr) / sizeof(int);
+}
+
+int main() {
+	int N, i;
+	int *p;
+	scanf("%d", &N);
+	p = (int*)calloc(N,sizeof(int));
+	for (i = 0; i < N; ++i) {
+		scanf("%d", &p[i]);
+	}
+
+	return 0;
+}
