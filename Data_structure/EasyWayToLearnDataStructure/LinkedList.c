9d1942edddcda039f05f6706be121a8d2c299684
Blogsoul <42922298+blogSoul@users.noreply.github.com>
2020-04-21 01:43:00 +0900

    Create LinkedList.c

---
+++ Data_structure/EasyWayToLearnDataStructure/LinkedList.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int element;
+typedef struct ListNode {
+	element data;
+	struct ListNode *link;
+}ListNode; // 구조체를 설정합니다.
+
+int main() {
+	ListNode *head = NULL;
+	return 0;
+}
